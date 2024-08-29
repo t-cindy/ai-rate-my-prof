6a034f50a043bf5ee6f49328e813b297cb3b5baf
ricardo fernandez <ricardo.fernandez12@upr.edu>
2024-08-28 21:03:10 -0400

    Added UI

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'pastel-pink': '#FFD1DC',
+        'pastel-yellow': '#FFEDB8',
+        'pastel-blue': '#A3CEF1',
+        'pastel-green': '#B2F0E8',
+        'pastel-green-dark': '#9CE3D3',
+      },
+      animation: {
+        blob: 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-20px, px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
